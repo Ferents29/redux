aefb2ae61e8458717048a75ca2d5fd7b68a2e321
VMFerents <vmferents@gmail.com>
2022-04-29 21:44:37 +0300

    actionCreator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
+import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
 
 function App() {
   const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
     const addCash = (cash) => {
       dispatch({type:'ADD_CASH',payload:cash})
     }
     const getCash = (cash) => {
         dispatch({type:'GET_CASH',payload:cash})
+    }
+    const addCustomer = (name) => {
+      let customer = {
+          id:Date.now(),
+          name:name,
+      }
+      dispatch(addCustomerAction(customer))
+    }
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer))
     }
   return (
     <div className="App">
@@ -16,7 +28,25 @@ function App() {
       <div style={{display:'flex',}}>
         <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
         <button onClick={() => getCash(Number(prompt()))}>Снять счет</button>
+        <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+        <button onClick={() => getCash(Number(prompt()))}>Удалить клиента</button>
       </div>
+        <div>
+            {customers.length > 0
+                ?
+                <div>
+                    {customers.map(customer =>
+                        <div onClick={() => removeCustomer(customer)}>
+                            {customer.name}
+                        </div>
+                    )}
+                </div>
+                :
+                <div style={{fontsize: '2rem',margin:10}}>
+                    Клиенты отсутствуют.
+                </div>
+            }
+        </div>
     </div>
   );
 }
